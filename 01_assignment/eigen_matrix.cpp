fa187d17efcebad787e45ec5e9fc43efc9929118
alvaroprovencio <alvaroproven@gmail.com>
2025-09-23 19:51:23 +0200

    eigen matrix

---
+++ 01_assignment/eigen_matrix.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "D:\eigen\eigen-master\Eigen\Dense"
+
+using Eigen::MatrixXi;
+
+int main() {
+    int n = 1000;
+    MatrixXi matrix_1 = MatrixXi::Random(n, n);
+    MatrixXi matrix_2 = MatrixXi::Random(n, n);
+    MatrixXi matrix_r = matrix_1 * matrix_2;
+}
