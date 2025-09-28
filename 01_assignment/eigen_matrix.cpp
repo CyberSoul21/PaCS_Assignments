5b25447db2b70d20751777f2c94a4bde72991855
cybersoul21 <wilson.jar21@gmail.com>
2025-09-29 00:22:15 +0200

    Completed

--- 01_assignment/eigen_matrix.cpp
+++ 01_assignment/eigen_matrix.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-#include "D:\eigen\eigen-master\Eigen\Dense"
-//#include "/home/javier/eigen/Eigen/Dense"
+//#include "D:\eigen\eigen-master\Eigen\Dense"
+#include "/home/javier/eigen/Eigen/Dense"
 using Eigen::MatrixXi;
 
 int main() {
-    int n = 1000;
+    int n = 2000;
     MatrixXi matrix_1 = MatrixXi::Random(n, n);
     MatrixXi matrix_2 = MatrixXi::Random(n, n);
     MatrixXi matrix_r = matrix_1 * matrix_2;
