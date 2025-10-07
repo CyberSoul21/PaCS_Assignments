30e1c4e0acf1415e66201234955dee611b05516b
alvaroprovencio <alvaroproven@gmail.com>
2025-10-07 17:47:37 +0200

    gettimeofday and strace

--- 02_assignment/get_time_of_day/standard_matrix_962449_960625.cpp
+++ 02_assignment/get_time_of_day/standard_matrix_962449_960625.cpp
@@ -5,10 +5,15 @@
 
 #include <iostream>
 #include <random>
+#include <sys/time.h>
 
 using namespace std;
 
 int main() {
+    struct timeval timestamp;
+    gettimeofday(&timestamp, NULL);
+    cout << "Start declaration and mem allocation. "<< "Seconds: " << timestamp.tv_sec << endl
+        << "Microseconds: " << timestamp.tv_usec << endl;
 
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -31,6 +36,10 @@ int main() {
         }
     }
     
+    gettimeofday(&timestamp, NULL);
+    cout << "End declaration, start multiplication. "<< "Seconds: " << timestamp.tv_sec << endl
+        << "Microseconds: " << timestamp.tv_usec << endl;
+    
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < n; j++)
@@ -41,5 +50,14 @@ int main() {
         }
     }
 
-    delete [] matrix_r, matrix_1, matrix_2;
+    gettimeofday(&timestamp, NULL);
+    cout << "End multiplication. "<< "Seconds: " << timestamp.tv_sec << endl
+        << "Microseconds: " << timestamp.tv_usec << endl;
+
+        delete [] matrix_r, matrix_1, matrix_2;
+
+    gettimeofday(&timestamp, NULL);
+    cout << "Last delete dynamic memory. "<< "Seconds: " << timestamp.tv_sec << endl
+        << "Microseconds: " << timestamp.tv_usec << endl;
+
 }
