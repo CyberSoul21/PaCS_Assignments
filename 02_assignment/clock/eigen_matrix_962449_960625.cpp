571a344ae3b9c8fdc090264591b65c5df2ab0828
cybersoul21 <wilson.jar21@gmail.com>
2025-10-07 17:54:09 +0200

    clock for eigen

--- 02_assignment/clock/eigen_matrix_962449_960625.cpp
+++ 02_assignment/clock/eigen_matrix_962449_960625.cpp
@@ -5,11 +5,21 @@
 
 #include <iostream>
 #include "/home/javier/eigen/Eigen/Dense"
+#include <ctime>
+#include <iomanip>
+
 using Eigen::MatrixXi;
+using namespace std;
 
 int main() {
+    clock_t time;
+    time = clock();
     int n = 100; //matrix dimension
     MatrixXi matrix_1 = MatrixXi::Random(n, n);
     MatrixXi matrix_2 = MatrixXi::Random(n, n);
     MatrixXi matrix_r = matrix_1 * matrix_2;
+
+    time = clock() - time;
+    cout << "Seconds: " << fixed << setprecision(4)
+        << ((float)time)/CLOCKS_PER_SEC << endl;
 }
