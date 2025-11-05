1cf487df06a9c652324339d4e2a20d2afcf6e5e3
cybersoul21 <wilson.jar21@gmail.com>
2025-11-05 10:06:21 +0100

    adding kahan

--- 03_assignment/pi_taylor_parallel_async_kahan.cc
+++ 03_assignment/pi_taylor_parallel_async_kahan.cc
@@ -6,28 +6,10 @@
 #include <thread>
 #include <utility>
 #include <vector>
+#include <future>
 
-using my_float = long double;
-
-void
-pi_taylor_chunk(std::vector<my_float> &output,
-        size_t thread_id, size_t start_step, size_t stop_step) {
-    
-    //(void) thread_id;
-    my_float one=-1;
-    my_float sum = 0.0f;
-    
-    (start_step % 2 == 0)?one = -1:one = 1;
-
-    for(size_t i = start_step; i < stop_step; i++){
-        one *= -1;
-        //output[i] = one / (2*i + 1);
-        sum += one / (2*i + 1);
-        //std::cout<<"r: "<<one / (2*i + 1)<<std::endl;
-        //std::cout<<"one: "<<one<<std::endl;
-    };
-    output[thread_id] = sum;
-}
+//using my_float = long double;
+using my_float = float;
 
 typedef struct {
     size_t large_chunk;
@@ -35,12 +17,36 @@ typedef struct {
     size_t split_item;
 } chunk_info;
 
+my_float
+pi_taylor_chunk(size_t start_step, size_t stop_step) {
+
+    my_float one = (start_step % 2 == 0) ? 1.0f : -1.0f;
+    my_float sum = 0.0f;
+    
+    // Variable to store the error
+    my_float c = 0.0f;
+
+
+    //(start_step % 2 == 0) ? one = 1 : one = -1; 
+
+    for(size_t i = start_step; i < stop_step; i++){
+
+        my_float y = one / (2*i + 1) - c;
+        my_float t = sum + y; 
+
+        c = (t - sum) - y; 
+        sum = t;
+        one *= -1;
+    };
+    return sum;
+}
+
+
 constexpr chunk_info
 split_evenly(size_t N, size_t threads)
 {
     return {N / threads + 1, N / threads, N % threads};
 }
-
 
 std::pair<size_t, size_t>
 get_chunk_begin_end(const chunk_info& ci, size_t index)
@@ -55,6 +61,7 @@ get_chunk_begin_end(const chunk_info& ci, size_t index)
     }
     return std::make_pair(begin, end);
 }
+
 
 std::pair<size_t, size_t>
 usage(int argc, const char *argv[]) {
@@ -77,38 +84,36 @@ usage(int argc, const char *argv[]) {
 
 int main(int argc, const char *argv[]) {
 
+
     auto ret_pair = usage(argc, argv);
     auto steps = ret_pair.first;
     auto threads = ret_pair.second;
 
-    my_float pi=0.0f;
-    //size_t pi=0.0f;
-    std::vector<my_float> partial_results(threads, 0.0f);
-    std::vector<std::thread> myThreads;
-    myThreads.reserve(threads);
+    my_float pi = 0.0f;
 
     // please complete missing parts
+    std::vector<std::future<my_float>> futureSums;
+    futureSums.reserve(threads);
+
     auto chunks = split_evenly(steps, threads);
-    //std::cout<<"Large chunks: "<<chunks.large_chunk<<"small chunks: "<<chunks.small_chunk<<" partial_results size: "<<partial_results.size()<<std::endl;
-    // ToDo : run several times and check median and deviation
 
     // launch the work
     auto start = std::chrono::steady_clock::now();  // Start timing
 
-    for(size_t i = 0; i < threads; ++i) {
+    for (size_t i = 0; i < threads; ++i) {
         auto begin_end = get_chunk_begin_end(chunks, i);
-        //std::cout<<"Begin: "<<begin_end.first<<" End: "<<begin_end.second<<std::endl;
-        myThreads.push_back(std::thread(pi_taylor_chunk, std::ref(partial_results), i, begin_end.first,begin_end.second));
+        futureSums.emplace_back(std::async(std::launch::async, pi_taylor_chunk,begin_end.first,begin_end.second));
     }
 
-    for(auto& t: myThreads) {
-        t.join();
-    }
+    // Variable to store the error
+    my_float c = 0.0f;
 
-    
-    for(auto r: partial_results) {
-        //std::cout<<"r: "<<r<<std::endl;
-        pi+=r;
+    for(auto &r : futureSums)
+    {
+        my_float y = r.get() - c;
+        my_float t = pi + y; 
+        c = (t - pi) - y; 
+        pi = t;     
     }
 
     pi*=4.0f;
@@ -116,13 +121,10 @@ int main(int argc, const char *argv[]) {
     auto end = std::chrono::steady_clock::now();    // End timing
 
     std::chrono::duration<double, std::milli> elapsed = end - start;
-    std::cout << "Time: " << elapsed.count() << " ms\n";
-
-
-
+    std::cout << "Time: " << elapsed.count() << " ms\n";    
 
     std::cout << "For " << steps << ", pi value: "
-        << std::setprecision(std::numeric_limits<long double>::digits10 + 1)
+        << std::setprecision(std::numeric_limits<my_float>::digits10 + 1)
         << pi << std::endl;
 }
 
