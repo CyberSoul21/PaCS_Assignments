e5b50fa02534ad80fc4f30a5a001460906c93fbe
cybersoul21 <wilson.jar21@gmail.com>
2025-11-13 10:21:26 +0100

    race condition fixed

--- 04_assignment/include/thread_pool.hpp
+++ 04_assignment/include/thread_pool.hpp
@@ -18,6 +18,12 @@ class thread_pool
   threadsafe_queue<std::function<void()> > work_queue;
   std::vector<std::thread> threads;
   join_threads joiner;
+
+
+  std::atomic<unsigned> active_tasks;  // Track running tasks
+  std::mutex completion_mutex;
+  std::condition_variable completion_cv;
+
   
   using task_type = void();
 
@@ -30,7 +36,14 @@ class thread_pool
 
       if(work_queue.try_pop(task)) // Blocks until task available // Try to get a task (non-blocking)
       {
+
         task(); // Execute the task
+        active_tasks--;
+
+        if(active_tasks == 0)  //check
+        {
+          completion_cv.notify_all();  // Notify waiters
+        } 
       }
       else // Queue is empty
       {
@@ -38,19 +51,32 @@ class thread_pool
         std::this_thread::yield(); //TODO: Use condition variable instead of yield() for better efficiency
       }
     }
+
+    // Process remaining tasks, it waits for the completion of all tasks in the queue
+    std::function<void()> task;
+    while(work_queue.try_pop(task))
+    {
+        task();
+        active_tasks--;
+        
+        if(active_tasks == 0)
+        {
+          completion_cv.notify_all();
+        }
+    }
   }
 
   public:
 
     thread_pool(size_t num_threads = std::thread::hardware_concurrency()):
-      done(false),joiner(threads)
+      done(false), active_tasks(0), joiner(threads)
     {
       try
       {
         for(unsigned i=0;i<num_threads;++i)
         {
           threads.push_back(
-          std::thread(&thread_pool::worker_thread,this));
+                std::thread(&thread_pool::worker_thread,this));
         }
       }
       catch(...)
@@ -71,11 +97,16 @@ class thread_pool
   {
       // wait for completion
       // active waiting
-      while (!work_queue.empty()) {
+      /*
+      while (!work_queue.empty()) {   //RACE CONDITION! (empty queue in not equal all tasks done)
           std::this_thread::yield();
-      }
-
-      done = true;
+      }*/
+      
+      //Wait for Queue to Empty
+      std::unique_lock<std::mutex> lock(completion_mutex);
+      completion_cv.wait(lock, [this] { 
+          return active_tasks == 0 && work_queue.empty(); 
+      });
   }
 
     //Template accepts any callable (function, lambda, functor)
