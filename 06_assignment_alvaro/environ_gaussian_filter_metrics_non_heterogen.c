ecfe7997f5fcc35bc91a865a60fbf6c86ae00bf0
cybersoul21 <wilson.jar21@gmail.com>
2025-12-19 14:45:31 +0100

    non-heterogeneous on GPU1

--- 06_assignment_alvaro/environ_gaussian_filter_metrics_non_heterogen.c
+++ 06_assignment_alvaro/environ_gaussian_filter_metrics_non_heterogen.c
@@ -152,9 +152,11 @@ int main(int argc, const char *argv[]) {
     
     // 4. Create contexts for both GPUs
     GPUContext gpu0_ctx;
-    gpu0_ctx.gpu_id = 0;
-    
-    gpu0_ctx.device_id = devices_ids[0][0];
+    //gpu0_ctx.gpu_id = 0;    
+    //gpu0_ctx.device_id = devices_ids[0][0];
+
+    gpu0_ctx.gpu_id = 1;  
+    gpu0_ctx.device_id = devices_ids[0][1];
     
     // Create context for GPU 0
     cl_context_properties properties0[] = {CL_CONTEXT_PLATFORM, (cl_context_properties)platforms_ids[0], 0};
